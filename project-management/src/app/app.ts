e34636f96f53d82fabc462a9a5206df869264075
R Gautham <gauthamramesh98270@gmail.com>
2026-04-20 17:40:47 +0530

    Completed Day1

---
+++ project-management/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Dashboard } from './features/dashboard/dashboard';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [Navbar, Sidebar, Dashboard],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('project-management');
+}
